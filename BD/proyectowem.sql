1ac9e27c81825538ab154bdb94580b88b1838dfe
cristian92927 <ct92927@gmail.com>
2020-08-15 17:04:19 -0500

    BD

--- BD/proyectowem.sql
+++ BD/proyectowem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2020 a las 23:44:51
+-- Tiempo de generación: 16-08-2020 a las 00:03:43
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.8
 
